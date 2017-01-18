be12202b69151aa81ead14fddbba3280df203d66
XieWei <yuanyuan756@126.com>
2017-01-18 11:25:18 +0800

    动态详情页回复弹出框

--- app/src/main/java/com/culturebud/ui/bhome/DynamicDetailActivity.java
+++ app/src/main/java/com/culturebud/ui/bhome/DynamicDetailActivity.java
@@ -1,13 +1,26 @@
 package com.culturebud.ui.bhome;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.culturebud.BaseActivity;
+import com.culturebud.BaseApp;
+import com.culturebud.CommonConst;
 import com.culturebud.R;
 import com.culturebud.adapter.DynamicDetailCommentAdapter;
 import com.culturebud.annotation.PresenterInject;
@@ -15,6 +28,7 @@ import com.culturebud.bean.BookCircleDynamic;
 import com.culturebud.bean.DynamicReply;
 import com.culturebud.contract.DynamicDetailContract;
 import com.culturebud.presenter.DynamicDetailPresenter;
+import com.culturebud.util.WidgetUtil;
 import com.culturebud.widget.RecyclerViewDivider;
 import com.google.gson.Gson;
 
@@ -26,16 +40,30 @@ import java.util.List;
 
 @PresenterInject(DynamicDetailPresenter.class)
 public class DynamicDetailActivity extends BaseActivity<DynamicDetailContract.Presenter>
-        implements DynamicDetailContract.View, DynamicDetailCommentAdapter.OnItemClickListener {
+        implements DynamicDetailContract.View, DynamicDetailCommentAdapter.OnItemClickListener, View.OnFocusChangeListener, BaseActivity.OnSoftKeyboardStateChangedListener {
     private static final String TAG = DynamicDetailActivity.class.getSimpleName();
     private BookCircleDynamic bcd;
     private RecyclerView rvReplies;
+
+    private PopupWindow pwReply;
+    private EditText etReplyInput;
+    private TextView tvSend;
+    private InputMethodManager imm;
+    private int screenHeight;
+
+    private BottomSheetDialog deleteDialog;
+    private TextView tvContent, tvDel, tvCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dynamic_detail);
         presenter.setView(this);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        screenHeight = dm.heightPixels;
+        imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
 
         rvReplies = obtainViewById(R.id.rv_replies);
         LinearLayoutManager llm = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -50,6 +78,7 @@ public class DynamicDetailActivity extends BaseActivity<DynamicDetailContract.Pr
         showBack();
         setTitle(R.string.dynamic_detail);
         initData();
+        addSoftKeyboardChangedListener(this);
     }
 
     private void initData() {
@@ -65,6 +94,69 @@ public class DynamicDetailActivity extends BaseActivity<DynamicDetailContract.Pr
             finish();
         }
         presenter.dynamicDetail(dynamicId);
+    }
+
+    private void initPopupWindow() {
+        if (pwReply == null) {
+            pwReply = new PopupWindow(this, null, R.style.PopupWindow);
+            View view = getLayoutInflater().inflate(R.layout.reply_input_pop, null);
+            etReplyInput = obtainViewById(view, R.id.et_reply_input);
+            etReplyInput.setMinLines(3);
+            tvSend = obtainViewById(view, R.id.tv_send);
+            pwReply.setContentView(view);
+            pwReply.setWidth(WindowManager.LayoutParams.MATCH_PARENT);
+            pwReply.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+            pwReply.setOutsideTouchable(true);
+            pwReply.setFocusable(true);
+            pwReply.setInputMethodMode(PopupWindow.INPUT_METHOD_FROM_FOCUSABLE);
+            pwReply.setSoftInputMode(PopupWindow.INPUT_METHOD_FROM_FOCUSABLE);
+            pwReply.setBackgroundDrawable(new BitmapDrawable());
+            etReplyInput.setOnFocusChangeListener(this);
+            tvSend.setOnClickListener(this);
+            obtainViewById(view, R.id.iv_at_friend).setOnClickListener(this);
+        }
+    }
+
+    private void showPop() {
+        initPopupWindow();
+        View dv = getWindow().getDecorView();
+        pwReply.showAtLocation(dv, Gravity.NO_GRAVITY, 0, dv.getHeight() / 2);
+        etReplyInput.setFocusable(true);
+        etReplyInput.requestFocus();
+    }
+
+    private void initDeleteDialog() {
+        if (deleteDialog == null) {
+            deleteDialog = new BottomSheetDialog(this);
+            deleteDialog.setContentView(R.layout.bottom_sheet_dialog);
+            deleteDialog.setCancelable(true);
+            deleteDialog.getWindow().findViewById(android.support.design.R.id.design_bottom_sheet)
+                    .setBackgroundResource(android.R.color.transparent);
+            tvContent = (TextView) deleteDialog.getWindow().findViewById(R.id.tv_opera_content);
+            tvDel = (TextView) deleteDialog.getWindow().findViewById(R.id.tv_del);
+            tvCancel = (TextView) deleteDialog.getWindow().findViewById(R.id.tv_cancel);
+            tvContent.setText("删除此条回复？删除后将不可恢复");
+            tvContent.setGravity(Gravity.CENTER);
+            WidgetUtil.setRawTextSize(tvContent, getResources().getDimensionPixelSize(R.dimen.dialog_opera_font_size));
+            tvCancel.setTextColor(Color.BLUE);
+            tvDel.setOnClickListener(v -> {
+//                presenter.deleteDynamicOrReply(currClickBcd.getDynamicId(), currDeleteType,
+//                        currDeleteType == CommonConst.DeleteType.TYPE_DYNAMIC ? currClickBcd.getDynamicId()
+//                                : currClickDr.getReplyId());
+                deleteDialog.dismiss();
+            });
+            tvCancel.setOnClickListener(v -> {
+                deleteDialog.dismiss();
+            });
+        }
+    }
+
+    private void showDeleteDialog(String content) {
+        initDeleteDialog();
+        if (!TextUtils.isEmpty(content)) {
+            tvContent.setText(content);
+        }
+        deleteDialog.show();
     }
 
     @Override
@@ -95,16 +187,73 @@ public class DynamicDetailActivity extends BaseActivity<DynamicDetailContract.Pr
         ((DynamicDetailCommentAdapter) rvReplies.getAdapter()).onThumbUp(dynamicId, result);
     }
 
+    private BookCircleDynamic currClickBcd;
+    private DynamicReply currClickDr;
+    private int currDeleteType = CommonConst.DeleteType.TYPE_DYNAMIC;
+
     @Override
     public void onItemClick(View v, int type, BookCircleDynamic bcd, DynamicReply dynamicReply) {
+        currClickBcd = null;
+        currClickDr = null;
+
         switch (type) {
             case DynamicDetailCommentAdapter.ITEM_CLICK_TYPE_THUMBUP:
                 presenter.thumbUP(bcd.getDynamicId());
                 break;
             case DynamicDetailCommentAdapter.ITEM_CLICK_TYPE_REPLY_DYNAMIC:
+                currClickBcd = bcd;
+                currClickDr = dynamicReply;
+                showPop();
+                etReplyInput.setHint("");
                 break;
             case DynamicDetailCommentAdapter.ITEM_CLICK_TYPE_REPLY_REPLY:
+                currClickBcd = bcd;
+                currClickDr = dynamicReply;
+                currDeleteType = CommonConst.DeleteType.TYPE_DYNAMIC_REPLY;
+                if (BaseApp.getInstance().getUser().getUserId() == dynamicReply.getUserId()) {
+                    showDeleteDialog("删除此条回复？删除后将不可恢复");
+                } else {
+                    showPop();
+                    etReplyInput.setHint("回复：" + currClickDr.getNickname());
+                }
                 break;
         }
+    }
+
+    private Handler handler = new Handler();
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (v == etReplyInput && hasFocus) {
+            handler.postDelayed(() -> {
+                runOnUiThread(() -> {
+                    imm.showSoftInput(etReplyInput, InputMethodManager.SHOW_FORCED);
+                });
+            }, 100);
+        }
+    }
+
+    @Override
+    public void onSoftKeyboardStateChanged(boolean isKeyBoardShow, int keyboardHeight) {
+        if (isKeyBoardShow) {
+            pwReply.update(0, screenHeight - (keyboardHeight + pwReply.getContentView()
+                            .getMeasuredHeight()),
+                    pwReply.getWidth(), pwReply.getHeight(), true);
+        } else {
+            pwReply.update(0, screenHeight - pwReply.getContentView().getMeasuredHeight(),
+                    pwReply.getWidth(), pwReply.getHeight(), true);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        removeSoftKeyboardChangedListener(this);
+        super.onDestroy();
     }
 }
